72a5c57013ea1ba87122d0f0611c38304bdb8f0d
MD Rahman <mdrahman1472@yahoo.com>
2017-10-15 16:24:07 -0400

    int and float variable rules are added

--- flask-tutorialpoint/5_variable_rules/variable_rules.py
+++ flask-tutorialpoint/5_variable_rules/variable_rules.py
@@ -9,6 +9,24 @@ app = Flask(__name__)
 def hello_name(name):
 	return 'Hello %s!' %name
 
+
+
+'''
+Inaddition to string default variable, rules can be constructed
+using int, float, and 
+path(accepts slashes used as directory separator character)
+'''
+
+# using int 
+@app.route('/blog/<int:postID>')
+def show_blog(postID):
+	return 'Blog Number %d' %postID
+
+#using float
+@app.route('/rev/<float:revNo>')
+def revision(revNo):
+	return 'Revision Number %f' %revNo
+
+
 if __name__ == '__main__':
 	app.run(debug=True)
-
