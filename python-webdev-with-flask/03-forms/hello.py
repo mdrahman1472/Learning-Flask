3ebc2cc743499d0a7d05a4bca3b831c98dc3f571
Md Rahman <mdrahman1472@yahoo.com>
2017-10-17 01:42:12 -0400

    form added

---
+++ python-webdev-with-flask/03-forms/hello.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	name = None
+	if request.method == "POST" and 'name' in request.form:
+		name = request.form['name']
+	return render_template('index.html', name=name)
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
+
+'''
+request.form is a python dictonary with the data entered by the users
+'''
